8ab5176e597dfd6fa7475211521337c38696931a
shaho <toofani@gmail.com>
2019-07-20 23:41:56 +0430

    Add cart reducer, actions

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase.utils";
 
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart/cart-dropdown.component";
+
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
@@ -28,7 +31,9 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,5 @@
+const CarttypeActions = {
+  TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
+};
+
+export default CarttypeActions;
