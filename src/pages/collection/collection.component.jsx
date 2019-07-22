536d00fce71e4f62b63df3e7fd3234023eece606
shaho <toofani@gmail.com>
2019-07-23 00:48:32 +0430

    Add collection component and its basic functionality

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import "./collection.styles.scss";
+
+const CollectionPage = ({ match }) => {
+  return (
+    <div className="collection">
+      <h1>Collection Page</h1>
+    </div>
+  );
+};
+
+export default CollectionPage;
