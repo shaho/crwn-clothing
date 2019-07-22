13906416fa81fe3cf37f9fabe54b8a97380c6754
shaho <toofani@gmail.com>
2019-07-22 18:28:17 +0430

    Setup shop folder in redux and its coreesponds reducer and selectors

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -7,6 +7,7 @@ import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 import directoryRducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 const persistConfig = {
   key: "root",
@@ -18,6 +19,7 @@ const rooReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   directory: directoryRducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rooReducer);
