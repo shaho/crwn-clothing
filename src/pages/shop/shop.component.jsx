1935687dfcae3fbdd447dbf85b3cc407465869ee
shaho <toofani@gmail.com>
2019-07-23 00:34:28 +0430

    Add small change to shop component - End of Sec. 10

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import CollectionOverview from "../../components/collections-overview/collections-overview.component";
 
-const ShopPage = ({ collections }) => {
+const ShopPage = () => {
   return (
     <div className="shop-page">
       <CollectionOverview />
